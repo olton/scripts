c7037694df3c8bda5b3d649fa0152cad82e454aa
Sergey Pimenov <sergey@pimenov.com.ua>
2021-10-02 20:26:44 +0300

    added Mina Monitor Cluster

--- mina/monitor/cluster/README.md
+++ mina/monitor/cluster/README.md
@@ -1,10 +1,10 @@
-# Mina Monitor Client Install
+# Mina Monitor Cluster Install
 
-The script is designed to quickly install Mina Monitor Client
+The script is designed to quickly install Mina Monitor Cluster
 
 ### Using
 ```shell
-curl -s https://raw.githubusercontent.com/olton/scripts/master/mina/monitor/client/install.sh | bash
+curl -s https://raw.githubusercontent.com/olton/scripts/master/mina/monitor/cluster/install.sh | bash
 ```
 
 ### Options
@@ -14,7 +14,7 @@ You can set a branch or tag where the script will take files and where these fil
 To  set branch use first argument for script and for target folder second.
 
 ```shell
-curl -s https://raw.githubusercontent.com/olton/scripts/master/mina/monitor/client/install.sh | bash -s -- websocket mina-monitor-client
+curl -s https://raw.githubusercontent.com/olton/scripts/master/mina/monitor/cluster/install.sh | bash -s -- websocket mina-monitor-cluster
 ```
 
 > If you need to set a target folder, you must define a branch.
---
+++ mina/monitor/cluster/install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+YELLOW="\033[33m"
+GREEN="\033[32m"
+
+echo -e "$GREEN Welcome to Mina Monitor Cluster installer!\033[0m"
+
+if [ -n "$1" ]
+then
+VER=$1
+else
+VER="master"
+fi
+
+if [ -n "$2" ]
+then
+TARGET=$2
+else
+TARGET="mina-monitor-cluster"
+fi
+
+echo -e "$GREEN We are installing Mina Monitor Cluster ${VER} branch\033[0m"
+echo -e "$GREEN into a ~/${TARGET} folder\033[0m"
+echo "So, let's go..."
+
+echo -e "$YELLOW Installing Mina Monitor Cluster...\033[0m"
+
+echo -e "$YELLOW Creating a target directory...\033[0m"
+
+cd ~
+mkdir -p ${TARGET}
+cd ${TARGET}
+
+echo -e "$YELLOW Downloading a required tarball...\033[0m"
+
+curl -L https://github.com/olton/mina-monitor-cluster/tarball/${VER} >> _.tar.gz
+
+echo -e "$YELLOW Extracting files...\033[0m"
+
+url=$(tar -tf _.tar.gz | head -n 1)
+tar --strip-components=2 -xf _.tar.gz ${url}src
+tar -xf _.tar.gz ${url}package.json ${url}README.md ${url}CHANGELOG.md ${url}babel.comfig.json
+
+echo -e "$YELLOW Deleting temporary files...\033[0m"
+
+rm _.tar.gz
+
+echo -e "$YELLOW Installing dependencies...\033[0m"
+
+npm install
+
+#echo -e "$YELLOW Creating config file...\033[0m"
+#node start --no-start
+
+echo ""
+echo -e "$GREEN Mina Monitor Client successfully installed...\033[0m"
+echo ""
