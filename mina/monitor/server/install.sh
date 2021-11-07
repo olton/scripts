cd8f1b4256d0e387e5928f01197c083908e635fd
Sergey Pimenov <sergey@pimenov.com.ua>
2021-11-07 13:19:23 +0200

    Mina Monitor Server: added installation service

--- mina/monitor/server/install.sh
+++ mina/monitor/server/install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+clear
+
 YELLOW="\033[33m"
 GREEN="\033[32m"
 
 echo -e "$GREEN Welcome to Mina Monitor Server installer!\033[0m"
 echo -e "$GREEN Copyright 2021 by Serhii Pimenov <serhii@pimenov.com.ua>\033[0m"
+read -p " If you are ready, press [Enter] key to start or Ctrl+C to stop..."
 
 TARGET="mina-monitor-server"
 BRANCH="master"
-SERVICE_TARGET_SYSTEM = "/etc/systemd/system/"
-SERVICE_TARGET_USER = "/usr/lib/systemd/user"
-SERVICE_TARGET = "system"
+SERVICE_TARGET_SYSTEM="/etc/systemd/system/"
+SERVICE_TARGET_USER="/usr/lib/systemd/user"
+SERVICE_TARGET="no"
+SERVICE_FILE="minamon.service"
 
-while getopts ":t:b:" opt
+while getopts ":t:b:s:" opt
 do
   case $opt in
     t) TARGET=$OPTARG;;
@@ -39,29 +43,36 @@ echo -e "$YELLOW Extracting files...\033[0m"
 url=$(tar -tf _.tar.gz | head -n 1)
 tar --strip-components=2 -xf _.tar.gz ${url}server
 
-echo -e "$YELLOW Deleting temporary files...\033[0m"
-
-rm _.tar.gz
-
+echo ""
 echo -e "$YELLOW Installing dependencies...\033[0m"
 npm install
 
+echo ""
 echo -e "$YELLOW Creating config file...\033[0m"
-node index --init
-
-echo -e "$YELLOW Install Monitor as service...\033[0m"
-sed -i "s/\/home\/user\//$HOME/g" 'minamon.service'
-sed -i "s/mina-monitor/$TARGET/g" 'minamon.service'
-
-if ["$SERVICE_TARGET" == 'system']; then
-  cp "minamon.serivce" "$SERVICE_TARGET_SYSTEM"
-else
-  cp "minamon.serivce" "$SERVICE_TARGET_USER"
+if node index --init &>/dev/null; then
+  echo -e "$GREEN Config file was created successfully\033[0m"
 fi
 
-echo -e "$GREEN Mina Monitor Server Service successfully installed.\033[0m"
+sed -i "s#/home/user/#$HOME/#g" "$SERVICE_FILE"
+sed -i "s#mina-monitor#$TARGET#g" "$SERVICE_FILE"
+
+if [ "$SERVICE_TARGET" != "no" ]; then
+  echo ""
+  echo -e "$YELLOW Install Monitor as service with a file $SERVICE_FILE...\033[0m"
+  SERVICE_TARGET_FOLDER=$([ "$SERVICE_TARGET" == "system" ] && echo "$SERVICE_TARGET_SYSTEM" || echo "$SERVICE_TARGET_USER")
+  echo -e "$YELLOW Now, we are copying service file $SERVICE_FILE to the $SERVICE_TARGET_FOLDER. This operation required sudo. \033[0m"
+  read -p " Press [Enter] key to continue or Ctrl+C to stop..."
+
+  if sudo cp "$HOME/$TARGET/$SERVICE_FILE" "$SERVICE_TARGET_FOLDER"; then
+    echo -e "$GREEN Mina Monitor Server Service successfully installed.\033[0m"
+  fi
+fi
 
 echo ""
-echo -e "$GREEN Mina Monitor Server successfully installed.\033[0m"
+echo -e "$YELLOW Deleting temporary files...\033[0m"
+rm _.tar.gz
+
+echo ""
+echo -e "$GREEN Mina Monitor Server successfully installed. Enjoy!\033[0m"
 echo ""
 
