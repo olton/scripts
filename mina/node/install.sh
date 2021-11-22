d5b44fd472f5baf806e0ae8a669c0f709acc997e
Sergey Pimenov <sergey@pimenov.com.ua>
2021-11-22 15:41:43 +0200

    added script for install Mina Node

--- mina/node/install.sh
+++ mina/node/install.sh
@@ -121,8 +121,8 @@ welcome() {
 
 install_pre_requirements() {
   msg "$CYAN Preparing OS...$NOFORMAT"
-  sudo apt-update -y update -qq
-  sudo apt-upgrade -y upgrade -qq
+  sudo apt-get -y update -qq
+  sudo apt-get -y upgrade -qq
   sudo apt --fix-broken install
   sudo apt-get install -y apt-transport-https ca-certificates gnupg
   sudo apt-get install -y curl htop mc net-tools unzip
